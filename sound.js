c71e943822eed26c5b35f07fb9312bf20735426e
Florian Thiévent <florianjeremias@gmail.com>
2025-09-02 15:28:28 +0200

    worked

--- sound.js
+++
@@ -1,9 +0,0 @@
-
-
-self.addEventListener('message', (e) => {
-    const context = new AudioContext();
-    const o = context.createOscillator();
-    o.frequency.setTargetAtTime(parseInt(e), context.currentTime, 0);
-    o.connect(context.destination);
-    o.start(0);
-})
